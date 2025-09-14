f2add49d659561ba2c8bdeb7ef42f4d59aff5f50
Seunga Kim <70243358+sinya3558@users.noreply.github.com>
2025-09-14 20:34:46 +0900

    25/09/14 [PGS] Lv1 연습문제풀이

--- SeungaKim/[PGS]/01_가장많이받은선물.py
+++ SeungaKim/[PGS]/01_가장많이받은선물.py
@@ -1,52 +1,40 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/258712
-'''
-두 사람이 서로 주고받은 기록이 있으면, 더 많이 준 사람이 다음 달 선물 1 +
-만약 주고받은 수가 같거나 기록이 없으면, 선물 지수가 더 큰 사람이 선물 1개 +
-선물 지수 = given - taken. if the same 선물 지수, nothing happens.
-rt_val : 가장 많이 받는 친구의 선물 수
-
-[제한 조건]
-- 2 ≤ friends의 길이 = 친구들의 수 ≤ 50
-friends의 원소는 친구의 이름을 의미하는 알파벳 소문자로 이루어진 길이가 10 이하인 문자열입니다.
-이름이 같은 친구는 없습니다.
-- 1 ≤ gifts의 길이 ≤ 10,000
-gifts의 원소는 "A B"형태의 문자열입니다. A는 선물을 준 친구의 이름을 B는 선물을 받은 친구의 이름을 의미하며 공백 하나로 구분됩니다.
-A와 B는 friends의 원소이며 A와 B가 같은 이름인 경우는 존재하지 않습니다. -> identical
-'''
 def solution(friends, gifts):
-    if not (2 <= len(friends) <= 50):
-        raise ValueError("invalid len of friends")
-    if not(1 <= len(gifts) <= 10000):
-        raise ValueError("invalid len of gifts")
-    answer = 0
-    fri_map = {name: idx for idx, name in enumerate(friends)}
-    nums = len(friends)
-    # calculate given - takers
-    gift_cnt = [[0] * nums in len(friends)]
-    given = [0] * nums
-    taken = [0] * nums
+    if not (2 <= len(friends) <= 50) or not (1 <= len(gifts) <= 10000):
+        raise ValueError("Invalid input")
 
+    n = len(friends)
+    # 아.. enumerate()
+    name_to_index = {name: i for i, name in enumerate(friends)}
+    
+    # init matrices & cnt
+    gift_matrix = [[0] * n for _ in range(n)]  # gift_matrix[i][j]: i -> j
+    given = [0] * n
+    received = [0] * n
+    
+    # 아하 따로 빼내도 되는구나.  gift list 먼저 처리해두기. nested for loop 따로 써도 됬었네
     for g in gifts:
-        a, b = g.split()    # A, B are seperated by blank
-        g_idx = fri_map(a)  # idx for giver
-        t_idx = fri_map(b)  # idx for taker
-        gift_cnt[g_idx][t_idx] += 1
-        given[g_idx] += 1
-        taken[t_idx] += 1
-
-    # cal 선물지수
-    gift_index = [given[i] - taken[i] for i in range(len(friends))]
-    num_friends = len(friends)
-    next_month = [0]* len(friends)
-
-    for i in range(len(num_friends)):
-        for j in range(i + 1, num_friends):
-            # when its greater,
-            if gift_cnt[i][j] > gift_cnt[j][i]:
-                next_month[i] += 1
-            # when its smaller
-            elif gift_cnt[i][j] < gift_cnt[j][i]:
-                next_month[j] += 1
-            # nothing happens if they've got the same 선물지수
-    answer = max(next_month)
+        a, b = g.split()
+        i, j = name_to_index[a], name_to_index[b]   # dict uses key value as idx
+        gift_matrix[i][j] += 1
+        given[i] += 1
+        received[j] += 1
+    
+    gift_status = [given[i] - received[i] for i in range(n)]    # 선물 지수
+    result = [0] * n
+    
+    # compare all unique pairs (i, j)
+    for i in range(n):
+        for j in range(i + 1, n):
+            if gift_matrix[i][j] > gift_matrix[j][i]:
+                result[i] += 1
+            elif gift_matrix[i][j] < gift_matrix[j][i]:
+                result[j] += 1
+            else:
+                if gift_status[i] > gift_status[j]:
+                    result[i] += 1
+                elif gift_status[i] < gift_status[j]:
+                    result[j] += 1
+                # if its equal, nothing happens
+    answer = max(result)
     return answer
